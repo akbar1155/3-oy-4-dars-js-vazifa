1ca8864a54da66bc58d63756d4254dcedf2f872c
Akbar <isroilovakbar49@gmail.com>
2023-01-17 16:40:15 +0500

    dfadsf

---
+++ main.js
@@ -0,0 +1,181 @@
+// 2-masala
+
+// K butun son berilgan Baho natijalarini chiqaruvchi dastur tuzing. 1-yomon, 2-qoniqarsiz, 3-qoniqarli, 4-yaxshi, 5 - alo
+
+// let a = 3
+// switch (a) {
+//     case 1:
+//         console.log("1-yomon");
+//         break;
+//     case 2:
+//         console.log("2-qoniqarsiz");
+//         break;
+//     case 3:
+//         console.log("3-qoniqarli");
+//         break;
+//     case 4:
+//         console.log(" 4-yaxshi");
+//         break;
+//     case 5:
+//         console.log("5 - alo");
+//         break;
+
+//     default:
+//         break;
+// }
+
+// 3-masala
+
+
+// Oy raqami berilgan Kiritilgan oy qaysi faslga tegishli ekanin chiqaruvchi dastur tuzing.M - s 2 - qish
+
+// let a = 12
+// switch (a) {
+//     case 1:
+//         console.log("qiw");
+//         break;
+//     case 2:
+//         console.log("qiw");
+//         break;
+//     case 3:
+//         console.log("bahor");
+//         break;
+//     case 4:
+//         console.log("bahor");
+//         break;
+//     case 5:
+//         console.log("bahor");
+//         break;
+//     case 6:
+//         console.log("yoz");
+//         break;
+//     case 7:
+//         console.log("yoz");
+//         break;
+//     case 8:
+//         console.log("yoz");
+//         break;
+//     case 9:
+//         console.log("kuz");
+//         break;
+//     case 10:
+//         console.log("kuz");
+//         break;
+//     case 11:
+//         console.log("kuz");
+//         break;
+//     case 12:
+//         console.log("qiw");
+//         break;
+//     default:
+//         break;
+// }
+
+
+
+// 4-masala
+
+// Oy raqami berilgan shu oyda nechta kun borligin aniqlovchi dastur tuzing
+
+
+// let a = 12
+// switch (a) {
+//     case 1:
+//         console.log("1-oy-yanvar-31");
+//         break;
+//     case 2:
+//         console.log("28");
+//         break;
+//     case 3:
+//         console.log("31");
+//         break;
+//     case 4:
+//         console.log("30");
+//         break;
+//     case 5:
+//         console.log("31");
+//         break;
+//     case 6:
+//         console.log("30");
+//         break;
+//     case 7:
+//         console.log("31");
+//         break;
+//     case 8:
+//         console.log("31");
+//         break;
+//     case 9:
+//         console.log("30");
+//         break;
+//     case 10:
+//         console.log("31");
+//         break;
+//     case 11:
+//         console.log("30");
+//         break;
+//     case 12:
+//         console.log("31");
+//         break;
+//     default:
+//         break;
+// }
+
+
+
+
+// 5-masala
+
+// a, b sonlar berilgan c amal ham berilgan a va b sonlar ustida amal bajaruvchi dastur tuzing
+
+// let a = 5
+// let b = 10
+// let c = 4
+
+// switch (c) {
+//     case 1:
+//         console.log(a + b);
+//         break;
+//     case 2:
+//         console.log(a * b);
+//         break;
+//     case 3:
+//         console.log(a - b);
+//         break;
+//     case 4:
+//         console.log(a / b);
+//         break;
+//     default:
+//         break;
+// }
+
+
+// 6-masala
+
+
+
+// Bahoga qarab stependiyani aniqlovchi dastur tuzing
+
+
+
+
+// let a = 5
+// switch (a) {
+//     case 1:
+//         console.log("0 som");
+//         break;
+//     case 2:
+//         console.log("0 som");
+//         break;
+//     case 3:
+//         console.log("500 000 som");
+//         break;
+//     case 4:
+//         console.log("600 000 som");
+//         break;
+//     case 5:
+//         console.log("700 000 som");
+//         break;
+
+//     default:
+//         break;
+// }
